94d9144c70d42e7805237ba6b00f5c3ea551eb55
יסמין לוי גיבוי <m0585948911@gmail.com>
2026-02-01 23:58:54 +0000

    בוא נטפל בהתראת פוש מחוץ לאפליקציה גם בדפדפן וגם באנדרואיד באמצעות האפי

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,8 @@
-// Using compat libraries for broader browser support in service workers.
-importScripts('https://www.gstatic.com/firebasejs/11.9.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/11.9.1/firebase-messaging-compat.js');
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDW7Qr46lj42gd1y2PUi_NigiEH1tjJ2c0",
   authDomain: "yasmin-beauty-diary.firebaseapp.com",
@@ -11,17 +12,23 @@ const firebaseConfig = {
   appId: "1:90023766755:web:0390d8df4b00fc58ba73d6"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icon-192x192.png' // A default icon
+    icon: payload.notification.image || "/logo-192.png",
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
